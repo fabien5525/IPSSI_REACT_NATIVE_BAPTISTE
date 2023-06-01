1157944511ed8e1084a9e34a7ea370521f5e051c
Tristan <tristan.chauvin.28@gmail.com>
2023-06-01 14:44:47 +0200

    Bottom bar + icon

--- client/App.js
+++ client/App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import OptionUtilisateur from './Page/OptionUtilisateur';
+import Jeux from './Page/Jeux';
+import InventaireUtilisateur from './Page/InventaireUtilisateur';
+
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen 
+      name="Jeux" 
+      component={Jeux} 
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Image source={require("./assets/joystick.png")} style={{ width: size, height: size, tintColor: color }} />
+        ),
+      }}
+      />
+      <Tab.Screen 
+      name="Inventaire"
+       component={InventaireUtilisateur}
+       options={{
+        tabBarIcon: ({ color, size }) => (
+          <Image source={require("./assets/bag.png")} style={{ width: size, height: size, tintColor: color }} />
+        ),
+      }}
+        />
+        <Tab.Screen $
+        name="Option" 
+        component={OptionUtilisateur}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image source={require("./assets/Gear.png")} style={{ width: size, height: size, tintColor: color }} />
+          ),
+        }}
+         />
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
