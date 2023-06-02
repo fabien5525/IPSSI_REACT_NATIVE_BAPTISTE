71c9a4114cbcf19c0de83ec5e16663682027385c
fabien CCDC <fcolard9@gmail.com>
2023-06-02 12:10:56 +0200

    connexion fix & jwt back

--- client/Page/Inscription.js
+++ client/Page/Inscription.js
@@ -1,184 +1,157 @@
-import { StyleSheet, Text, View, Image,Modal,Button,TextInput, FlatList,Pressable} from 'react-native';
+import { StyleSheet, Text, View, Image, Modal, Button, TextInput, FlatList, Pressable } from 'react-native';
 import { useState } from 'react';
 
-export default function Inscription({navigation}) {
+export default function Inscription({ navigation }) {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [image, setImage] = useState('https://picsum.photos/200/300');
-    const [selectedImageId, setSelectedImageId] = useState(null);
+  const [email, setEmail] = useState('');
+  const [image, setImage] = useState('https://picsum.photos/200/300');
+  const [selectedImageId, setSelectedImageId] = useState(null);
 
-    const [Touteimage, setTouteImage] = useState([
-        {
-            id: 1,
-            image: 'https://picsum.photos/200/300',
-        },
-        {
-            id: 2,
-            image: 'https://picsum.photos/200/400',
-        },
-        {
-            id: 3,
-            image: 'https://picsum.photos/200/500',
-        },
-        {
-            id: 4,
-            image: 'https://picsum.photos/200/500',
-        },
-        {
-            id: 5,
-            image: 'https://picsum.photos/200/500',
-        },        {
-            id: 6,
-            image: 'https://picsum.photos/200/500',
-        },        {
-            id: 7,
-            image: 'https://picsum.photos/200/500',
-        },        {
-            id: 8,
-            image: 'https://picsum.photos/200/500',
-        },
-    ]);
+  const avatarDispo = [
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/200/400',
+    'https://picsum.photos/200/500',
+    'https://picsum.photos/200/500',
+    'https://picsum.photos/200/500',
+    'https://picsum.photos/200/500',
+    'https://picsum.photos/200/500',
+    'https://picsum.photos/200/500'
+  ];
 
-    const GoToConnexion = () => {
-        navigation.navigate('Connexion')
+  const GoToConnexion = () => {
+    navigation.navigate('Connexion')
+  }
+
+  const renderDisplayImage = ({ item, index }) => {
+    return (
+      <Pressable key={index} onPress={() => setImage(item)}>
+        <Image source={{ uri: item }}
+          style={[
+            styles.thumbnailImage,
+            selectedImageId === item.id && styles.highlightedImage,
+          ]} />
+      </Pressable>
+    )
+  }
+
+  const VerificationInsciption = async () => {
+    const req = await fetch('http://5525.fr:19001/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username, password: password, email: email, image: image }),
+    });
+    const status = await req.status;
+    const res = await req.json();
+
+    if (status === 200) {
+      alert('Inscription réussi');
+      navigation.navigate('Connexion');
+    } else {
+      alert(res.message);
     }
+  }
 
-    const handleImage = (item) => {
-        setSelectedImageId(item.id);
-        }
-    const renderDisplayImage = ({ item }) => {
-        return(
-            <Pressable onPress={() => handleImage(item)}>
-              <Image source={{ uri: item.image }}
-                style={[
-                    styles.thumbnailImage,
-                    selectedImageId === item.id && styles.highlightedImage,
-                  ]} />
-          </Pressable>
-        )
-    }
-
-    const VerificationInsciption = () => {
-        fetch('http://5525.fr:19001/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username: username, password: password, email: email, image: image }),
-        })
-        .then((response) => {
-            console.log(response)
-            return response.json();
-          })
-          .then((data) => {
-            // Traiter les données de la réponse
-            console.log(data.data.error);
-          })
-          .catch((error) => {
-            // Gérer les erreurs
-            console.error(error);
-          });
-    }
-
-return (
+  return (
     <View style={styles.container}>
 
-        <Text style={styles.modalText}>Identifiant</Text>
-        <TextInput 
-        placeholder="Username" 
+      <Text style={styles.modalText}>Identifiant</Text>
+      <TextInput
+        placeholder="Username"
         style={styles.modalInput}
         onChangeText={(text) => setUsername(text)}
-         />
+      />
 
-        <Text style={styles.modalText}>Mot de passe </Text>
-        <TextInput 
-        placeholder="password" 
-        style={styles.modalInput} 
+      <Text style={styles.modalText}>Mot de passe </Text>
+      <TextInput
+        placeholder="password"
+        style={styles.modalInput}
         onChangeText={(text) => setPassword(text)}
-        />
-        <Text style={styles.modalText}>Confirmation mot de passe</Text>
-        <TextInput
-         placeholder="password" 
-         style={styles.modalInput}
+      />
+      <Text style={styles.modalText}>Confirmation mot de passe</Text>
+      <TextInput
+        placeholder="password"
+        style={styles.modalInput}
         onChangeText={(text) => setPasswordConfirm(text)}
-          />
+      />
 
-        <Text style={styles.modalText}>E-mail</Text>
-        <TextInput 
-        placeholder="email" 
-        style={styles.modalInput} 
+      <Text style={styles.modalText}>E-mail</Text>
+      <TextInput
+        placeholder="email"
+        style={styles.modalInput}
         onChangeText={(text) => setEmail(text)}
-        />
+      />
 
-        <View  style={styles.bloc}>
+      <View style={styles.bloc}>
         <Text style={styles.modalText}>Image de profile</Text>
         <FlatList
-                horizontal={true}
-              data={Touteimage}
-              renderItem={renderDisplayImage}
-              keyExtractor={(item) => item.id.toString()}/>
-        </View>
+          horizontal={true}
+          data={avatarDispo}
+          renderItem={renderDisplayImage}
+          keyExtractor={(item) => item.id.toString()} />
+      </View>
 
-        <View style={styles.LesBoutons}>
-        <Button title="Se connecter" style={styles.modalButton} onPress={() => GoToConnexion()}/>
-        <Button title="S'inscrire" style={styles.modalButton} onPress={() => VerificationInsciption()}/>
-        </View>
+      <View style={styles.LesBoutons}>
+        <Button title="Se connecter" style={styles.modalButton} onPress={() => GoToConnexion()} />
+        <Button title="S'inscrire" style={styles.modalButton} onPress={() => VerificationInsciption()} />
+      </View>
     </View>
-)
+  )
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent:'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-    modalInput: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10,
-        marginBottom: 10,
-        width: '80%',
-        borderRadius: 5,
-      },
-      modalText: {
-        fontSize: 20,
-        marginBottom: 10,
-      },
-      thumbnailImage: {
-        width: 50,
-        height: 50,
-        borderRadius: 40, // Pour obtenir une image ronde
-        marginRight: 16,
-      },
+  modalInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    width: '80%',
+    borderRadius: 5,
+  },
+  modalText: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  thumbnailImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 40, // Pour obtenir une image ronde
+    marginRight: 16,
+  },
 
-      LesBoutons: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent : 'space-between',
-        width: '80%',
-        padding: 10,
-        borderRadius: 10,
-        margin : 10,
-        },
-        bloc: {
-            alignItems: 'center',
-            maxHeight: 100, // Hauteur minimale nécessaire pour afficher les images de profil
-          },
-          flatListContentContainer: {
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-          },
-          highlightedImage: {
-            borderWidth: 2,
-            borderColor: 'blue',
-          },
+  LesBoutons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '80%',
+    padding: 10,
+    borderRadius: 10,
+    margin: 10,
+  },
+  bloc: {
+    alignItems: 'center',
+    maxHeight: 100, // Hauteur minimale nécessaire pour afficher les images de profil
+  },
+  flatListContentContainer: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  highlightedImage: {
+    borderWidth: 2,
+    borderColor: 'blue',
+  },
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import { sign } from 'jsonwebtoken';
+import User from './database/models/user';
+import { hash, compare } from 'bcrypt';
 
+import { secret } from '../config/config.json';
 const PORT = 19001;
 
 const app = express();
@@ -9,14 +13,70 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.post('/register', async (req, res) => {
+    console.log("POST /register");
+
+    const { email, password, pseudo, avatar } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        res.status(400).send({ message: 'Bad request' });
+        return;
+    }
+
+    const user = new User();
+    user.email = email;
+    user.password = await hash(password, 8);
+    user.role = 'user';
+    user.pseudo = pseudo;
+    user.avatar = avatar;
+    await user.save();
+
+    sign({ id: user.id }, secret, { expiresIn: '72h' }, (err: any, token: any) => {
+        if (err) {
+            return res.status(500).send({ message: 'Internal server error' });
+        }
+        return res.status(200).send({ token });
+    });
+
+    return res.status(400).send({ message: 'Error while sign' });
+});
+
 app.post('/login', async (req, res) => {
     console.log("POST /login");
-    res.status(401).send({ message: 'OK' });
+
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        return res.status(400).send({ message: 'Bad request' });
+    }
+
+    const user = await User.findOne({
+        where: {
+            email
+        }
+    });
+
+    if (!user) {
+        return res.status(401).send({ message: 'Email not matching' });
+    }
+
+    const allowed = await compare(password, user.password);
+
+    if (allowed) {
+        sign({ id: user.id }, secret, { expiresIn: '72h' }, (err: any, token: any) => {
+            if (err) {
+                return res.status(500).send({ message: 'Internal server error' });
+            }
+            return res.status(200).send({ token });
+        });
+    } else {
+        return res.status(401).send({ message: 'Password not matching' });
+    }
 });
 
 app.get('*', async (req, res) => {
     console.log("GET *");
-    res.status(404).send({ message: 'Not found' });
+    return res.status(404).send({ message: 'Not found' });
 });
 
 app.listen(PORT, () => {
